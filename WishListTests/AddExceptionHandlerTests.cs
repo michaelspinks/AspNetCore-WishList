3b7d552e7b7a3f6b5d4f218a3c41a246b3a58d32
Mike Spinks <mikespinks@gmail.com>
2020-02-15 14:44:56 +0000

    Add Controller and Add Exception Handler.

--- WishListTests/AddExceptionHandlerTests.cs
+++ WishListTests/AddExceptionHandlerTests.cs
@@ -12,15 +12,11 @@ namespace WishListTests
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Startup.cs";
             string file;
 
-            // Create an instance of StreamReader to read from a file.
-            // The using statement also closes the StreamReader.
             using (var streamReader = new StreamReader(filePath))
             {
-                // Reads all characters from the current position until the end of the stream. 
                 file = streamReader.ReadToEnd();
             }
 
-            //Checks that the startup.cs file contains the string
             Assert.True(file.Contains("app.UseDeveloperExceptionPage();"), "`Startup.cs`'s `Configure` did not contain a call to `UseDeveloperExceptionPage`.");
             Assert.True(file.Contains(@"app.UseExceptionHandler(""/Home/Error"")"), "`Startup.cs`'s `Configure` did not contain a call to `UseExceptionHandler` that redirects to the `Home.Error` action.");
         }

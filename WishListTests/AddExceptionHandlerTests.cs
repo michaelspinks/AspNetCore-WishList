bf7422a049b6a5ee0a7372285d8de7a3d3a2d45c
Mike Spinks <mikespinks@gmail.com>
2020-02-15 14:32:35 +0000

    Add new View

--- WishListTests/AddExceptionHandlerTests.cs
+++ WishListTests/AddExceptionHandlerTests.cs
@@ -8,15 +8,24 @@ namespace WishListTests
         [Fact(DisplayName = "Configure Exception Handling @configure-exception-handling")]
         public void UseDeveloperExceptionPageTest()
         {
+
             var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Startup.cs";
             string file;
+
+            // Create an instance of StreamReader to read from a file.
+            // The using statement also closes the StreamReader.
             using (var streamReader = new StreamReader(filePath))
             {
+                // Reads all characters from the current position until the end of the stream. 
                 file = streamReader.ReadToEnd();
             }
 
+            //Checks that the startup.cs file contains the string
             Assert.True(file.Contains("app.UseDeveloperExceptionPage();"), "`Startup.cs`'s `Configure` did not contain a call to `UseDeveloperExceptionPage`.");
             Assert.True(file.Contains(@"app.UseExceptionHandler(""/Home/Error"")"), "`Startup.cs`'s `Configure` did not contain a call to `UseExceptionHandler` that redirects to the `Home.Error` action.");
         }
     }
 }
+
+// https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader.readtoend?view=netframework-4.8
+// https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader?view=netframework-4.8#examples
